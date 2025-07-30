b158a3ea840c6e99aa368e88f1f2ef994e336811
1cJulio <125521459+1cJulio@users.noreply.github.com>
2025-07-30 13:35:58 -0300

    @jurin

---
+++ script.js
@@ -0,0 +1,6 @@
+function inserirNome(){
+    let nomeusuario = prompt("qual seu nome");
+      let elemento = document.querySelector("nome-usuario");
+      console.log(elemento;)
+      elemento.textContent = nomeusuario;
+}
